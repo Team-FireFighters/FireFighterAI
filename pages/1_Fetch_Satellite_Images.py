088a500e8829c93b9544b4eec167cf8ab7e22ace
Rishit Chugh <rishitchugh@gmail.com>
2024-03-10 18:38:07 +0530

    why dont tf work with st aaahh

--- pages/1_Fetch_Satellite_Images.py
+++ pages/1_Fetch_Satellite_Images.py
@@ -17,7 +17,17 @@ locations = get_file_names_without_extension(folder_path)
 st.title("Fetch Latest Satellite Images")
 
 chosen_locations = st.multiselect('Choose your location', locations)
-if st.button('View Latest satellite View'):
-    
-    for image in chosen_locations:
-        st.image(f'{folder_path}/{image}.png', caption=image, use_column_width=True)
+if st.button('View Latest Satellite View'):
+    # Split the screen into two columns
+    col1, col2 = st.columns(2)
+
+    # Iterate through the chosen locations and display images in columns
+    for index, image in enumerate(chosen_locations):
+        if index % 2 == 0:  # Even index, display in col1
+            with col1:
+                st.image(f'{folder_path}/{image}.png', caption=image, use_column_width=True)
+        else:  # Odd index, display in col2
+            with col2:
+                st.image(f'{folder_path}/{image}.png', caption=image, use_column_width=True)
+
+st.link_button('Detect Fire in Satellite Images', 'http://localhost:5005/')
