892af2550d4da46f6bb55649ad5814e176c7ca04
Rishit Chugh <rishitchugh@gmail.com>
2024-03-13 22:12:04 +0530

    fixed yolo cli

--- pages/3_detect_fire_through_video.py
+++ pages/3_detect_fire_through_video.py
@@ -2,6 +2,8 @@ import streamlit as st
 import os
 st.title("Detect Fire in Camera Feed")
 HOME = "/Users/rishit/Documents/VIT/SEM5_point_5/EPICS"
+import ultralytics
+ultralytics.checks()
 os.system(f'cd {HOME}')
 os.system('!yolo task=detect mode=predict model=/Users/rishit/Documents/VIT/SEM5_point_5/EPICS/best.pt conf=0.25 source=/Users/rishit/Documents/VIT/SEM5_point_5/EPICS/forestfire.mp4 save=True')
 
